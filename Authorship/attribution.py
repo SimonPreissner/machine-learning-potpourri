2e0e37fef0056551a70bb92a22cbbd58f74c10c9
minimalparts <aurelie.herbelot@cantab.net>
2018-03-19 18:29:48 +0100

    Corrected Naive Bayes.

--- Authorship/attribution.py
+++ Authorship/attribution.py
@@ -1,67 +1,61 @@
+import os
 import sys
 import math
+from utils import process_document_words, process_document_ngrams
 
-vocab={}
-ngrams_dicts=[]
-alpha=0.0001
+ngram_length = 4
+alpha = 0.000001
+
+def process_collection():
+    '''Take table of authors and associated document frequencies
+    to calculate probability of each author.'''
+    author_probabilities = {}
+    author_freqs = {}
+    author_wordcounts = {}
+    files = [os.path.join('./data/training/', f) for f in os.listdir('./data/training/') if os.path.isfile(os.path.join('./data/training/', f))]
+    num_documents = len(files)
+    for f in files:
+        #author, doc_length, words = process_document_words(f)
+        author, doc_length, words = process_document_ngrams(f, ngram_length)
+        if author in author_probabilities:
+            author_probabilities[author] += 1
+            author_wordcounts[author] += doc_length
+            for w,f in words.items():
+                if w in author_freqs[author]:
+                    author_freqs[author][w] += f
+                else:
+                    author_freqs[author][w] = f
+        else:
+            author_probabilities[author] = 1
+            author_wordcounts[author] = doc_length
+            author_freqs[author] = words
+    for author,doc_freq in author_probabilities.items():
+        author_probabilities[author] = doc_freq / num_documents
+    #print(author_probabilities,author_wordcounts, author_freqs.keys())
+    return author_probabilities, author_wordcounts, author_freqs
 
 
-def fill_vocab(author_ngrams):
-    for ngram,freq in author_ngrams.items():
-      if ngram in vocab:
-        vocab[ngram]+=freq
-      else:
-        vocab[ngram]=freq
-
-
-
-def read_ngrams(filename):
-  ngrams={}
-  f=open(filename,'r')
-  for l in f:
-    l=l.rstrip('\n')
-    ngram=l.split("\t")[0]
-    freq=int(l.split("\t")[1])
-    ngrams[ngram]=freq
-  f.close()
-  fill_vocab(ngrams)
-  return ngrams
-
-def naive_bayes(test_ngrams,author):
-  nb=0
-  for ngram,freq in test_ngrams.items():
-    if ngram in ngrams_dicts[author]:
-      for i in range(freq):
-        nb=nb+math.log(ngrams_dicts[author][ngram])
-    else:
-      if ngram in vocab:
-        for i in range(freq):
-          nb=nb+math.log(float(alpha)/float(vocab[ngram]+alpha))
-  print nb
-
+def naive_bayes(test_words, author_freqs, author_probabilities, author):
+    '''Run Naive Bayes with test_ngrams = new text\
+    and author = particular value for author.'''
+    nb=0
+    for w,f in test_words.items():  # For each word in the test document
+        w_freq = 0
+        if w in author_freqs[author]:
+            w_freq = author_freqs[author][w]
+        for i in range(f):
+            '''Add to score the log of P(word|author)'''
+            nb += math.log((w_freq + alpha) / (author_wordcounts[author] * (1 + alpha)))
+    nb += math.log(author_probabilities[author])
+    print("NB score for author",author,":",nb)
 
 
 #Training
-#ngrams_dicts.append(read_ngrams("wind.ngrams"))
-#ngrams_dicts.append(read_ngrams("junglebook.ngrams"))
-#ngrams_dicts.append(read_ngrams("alice.ngrams"))
-#ngrams_dicts.append(read_ngrams("pride.ngrams"))
-
-ngrams_dicts.append(read_ngrams("wind.words"))
-ngrams_dicts.append(read_ngrams("junglebook.words"))
-ngrams_dicts.append(read_ngrams("alice.words"))
-ngrams_dicts.append(read_ngrams("pride.words"))
+author_probabilities, author_wordcounts, author_freqs = process_collection()
 
 #Testing
-#test_ngrams=read_ngrams("emma.ngrams")
-test_ngrams=read_ngrams("emma.words")
+#author, doc_length, words = process_document_words(sys.argv[1])
+author, doc_length, words = process_document_ngrams(sys.argv[1], ngram_length)
 
-#Calculate ngram probabilities for each author in ngrams_dicts
-for d in ngrams_dicts:
-  for ngram,freq in d.items():
-    d[ngram]=float(freq+alpha)/float(vocab[ngram]+alpha)
-
-naive_bayes(test_ngrams,0)
-naive_bayes(test_ngrams,1)
-naive_bayes(test_ngrams,2)
-naive_bayes(test_ngrams,3)
+for a in list(author_probabilities.keys()):
+     naive_bayes(words, author_freqs, author_probabilities, a)
---
+++ Authorship/utils.py
@@ -0,0 +1,43 @@
+
+def process_document_words(filename):
+    words={}
+    doc_length = 0
+    f=open(filename,'r', encoding='utf-8')
+    c = 0
+    for l in f:
+        l=l.rstrip('\n')
+        if c < 1:
+            #print(l)
+            author = l.replace("#Author: ","")
+        else:
+            for w in l.split():
+                if w in words:
+                    words[w]+=1
+                else:
+                    words[w]=1
+                doc_length += 1
+        c+=1
+    f.close()
+    return author, doc_length, words
+
+def process_document_ngrams(filename, n):
+    ngrams={}
+    doc_length = 0
+    f=open(filename,'r', encoding='utf-8')
+    c = 0
+    for l in f:
+        l=l.rstrip('\n')
+        if c < 1:
+            #print(l)
+            author = l.replace("#Author: ","")
+        else:
+            for i in range(len(l)-n):
+                ngram=l[i:i+n]
+                if ngram in ngrams:
+                    ngrams[ngram]+=1
+                else:
+                    ngrams[ngram]=1
+                doc_length += 1
+        c+=1
+    f.close()
+    return author, doc_length, ngrams
