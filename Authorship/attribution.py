2427d3501ca87836918ecc508f617b912eee13c6
minimalparts <aurelie.herbelot@cantab.net>
2018-03-26 10:42:36 +0200

    Added credits

--- Authorship/attribution.py
+++ Authorship/attribution.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+'''This version integrates sys.path and argument
+checks by Patrizio Bellan.'''
 
 from __future__ import division
 from __future__ import unicode_literals
